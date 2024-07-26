7d0142cd51e237d2e5b46d2233783d54f814a7f8
MaryamSalehi <tiamsalehi22148@gmail.com>
2024-07-26 17:00:11 +0330

    Add project files.

---
+++ BookPresenter/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+using BookPresenter.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BookPresenter.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        public List<BookViewModel> books = new List<BookViewModel>();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(BookViewModel book)
+        {
+            books.Add(book);
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BookPresenter/Models/BookViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BookPresenter.Models
+{
+    public class BookViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
